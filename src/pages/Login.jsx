deb99b356d1041dcbeb35b07252bec52fa720373
salah <salahakon1998@gmail.com>
2024-10-14 15:03:48 +0100

    Implement responsiveness for login page

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -27,31 +27,33 @@ export default function Login() {
 	return (
 		<main className={styles.login}>
 			<PageNav />
-			<form className={styles.form} onSubmit={handleSubmit}>
-				<div className={styles.row}>
-					<label htmlFor="email">Email address</label>
-					<input
-						type="email"
-						id="email"
-						onChange={(e) => setEmail(e.target.value)}
-						value={email}
-					/>
-				</div>
+			<div>
+				<form className={styles.form} onSubmit={handleSubmit}>
+					<div className={styles.row}>
+						<label htmlFor="email">Email address</label>
+						<input
+							type="email"
+							id="email"
+							onChange={(e) => setEmail(e.target.value)}
+							value={email}
+						/>
+					</div>
 
-				<div className={styles.row}>
-					<label htmlFor="password">Password</label>
-					<input
-						type="password"
-						id="password"
-						onChange={(e) => setPassword(e.target.value)}
-						value={password}
-					/>
-				</div>
+					<div className={styles.row}>
+						<label htmlFor="password">Password</label>
+						<input
+							type="password"
+							id="password"
+							onChange={(e) => setPassword(e.target.value)}
+							value={password}
+						/>
+					</div>
 
-				<div>
-					<Button type="primary">Login</Button>
-				</div>
-			</form>
+					<div>
+						<Button type="primary">Login</Button>
+					</div>
+				</form>
+			</div>
 		</main>
 	);
 }
